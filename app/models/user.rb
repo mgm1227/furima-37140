6401cb2a8148e845bedbbf38a722912d7f070af9
mgm1227 <chory.chory.c12@gmail.com>
2022-02-20 11:24:32 +0900

    user_バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         
+  validates :nickname, presence: true
+  validates :email   , presence: true
+  validates :name_first, presence: true
+  validates :name_last, presence: true
+  validates :name_first_kana, presence: true
+  validates :name_last_kana, presence: true
+  validates :birthday, presence: true
+
+  has_many :itmes
+  has_many :oders
 end
