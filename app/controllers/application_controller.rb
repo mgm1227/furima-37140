7fedcbb315f12824494c6265a5ab245e0533c432
mgm1227 <chory.chory.c12@gmail.com>
2022-02-22 22:45:31 +0900

    user_ログインログアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
     private
   def configure_permitted_parameters  # メソッド名は慣習
     # deviseのUserモデルにパラメーターを許可
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :name_family, :name_first, :name_family_kana, :name_first_kana, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :name_family, :name_first, :name_family_kana, :name_first_kana, :birthday])
   end
 end
 
