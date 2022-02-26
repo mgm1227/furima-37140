3758543277a16da527e26a4403444f5f66772132
mgm1227 <chory.chory.c12@gmail.com>
2022-02-26 18:02:36 +0900

    商品出品機能_ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,17 @@
 class Item < ApplicationRecord
 
-  
-
   belongs_to :user
   #has_one    :order
   has_one_attached :image
+
+  extend ActiveHash: :Associations: :ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :detail
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :day_to_ship
+
+  validates :item_name, :item_price, :description, prefecture: true
+  validates :category_id, :detail_id, :shipping_cost_id, :prefecture_id, :day_to_ship_id, numericality: { other_than: 1 }
+  
 end
