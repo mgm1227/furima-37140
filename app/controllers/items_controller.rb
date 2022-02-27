6f712eff8ecebaaa344426d6eb3952add7b93a2a
mgm1227 <chory.chory.c12@gmail.com>
2022-02-27 11:32:00 +0900

    商品出品機能_バリデーション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,11 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     
   end
+
+  
 
   private
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -12,6 +12,7 @@ class Item < ApplicationRecord
   belongs_to :day_to_ship
 
   validates :item_name, :item_price, :description, prefecture: true
+  validates :image, prefecture: true
   validates :category_id, :detail_id, :shipping_cost_id, :prefecture_id, :day_to_ship_id, numericality: { other_than: 1 }
-  
+  validates :price, prefecture: true, format: { with:/\A[0-9]{3,7}\z/, message: 'is invalid. Input half-width characters' }
 end
