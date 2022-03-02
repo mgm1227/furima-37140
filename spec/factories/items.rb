698fe164ff72eb25d75d92d8fb082d9eb4c1a241
mgm1227 <chory.chory.c12@gmail.com>
2022-03-03 00:02:03 +0900

    商品出品機能_単体テスコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
+    item_name          {'あいue0'}
+    item_price         { 12345678 }
+    description        {Faker::Lorem.sentence}
+    category_id        { 2 }
+    detail_id          { 2 }
+    shipping_cost_id   { 2 }
+    prefecture_id      { 2 }
+    day_to_ship_id     { 2 }
+    association :user
     
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/mei.png'), filename: 'mei.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,55 @@
 require 'rails_helper'
-
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品機能' do
+    context '商品が出品できる' do
+      it '商品が出品登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+    context '商品が出品できない' do
+      it '画像が空では登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it '商品名が空では登録できない' do
+        @item.item_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
+      end
+      it '商品説明が空では登録できない' do
+        
+      end
+      it 'カテゴリーが空だと登録できない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category is not a number")
+      end
+      it '商品の状態が空では登録できない' do
+        @item.detail_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Detail is not a number")
+      end
+      it '配送料負担が空では登録できない' do
+        @item.shipping_cost_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping cost is not a number")
+      end
+      it '発送元地域が空では登録できない' do
+        @item.prefecture_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture is not a number")
+      end
+      it '価格が全角数字では登録できない' do
+        @item.item_price = '１２３４'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item price is invalid. Input half-width characters")
+      end
+    end
+  end
+
 end
